75ec6f60525bfdbb7ff696702fbd1fb9729dbf4c
RealSwagus <Oisinkiely200@gmail.com>
2026-08-07 02:12:07 -0700

    Changed movement code
    Added more complex and customizable player movement code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed: int = 175
+var acceleration: int = 700
+var friction = 900
+var jump_strength = -350
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	apply_gravity(delta)
+	handle_jump()
+	
+	# move left and right
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	if input_axis != 0:
+		sprite.flip_h = (input_axis < 0) # flips sprite depending on move axis
+		velocity.x = move_toward(velocity.x, speed * input_axis, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	
 	move_and_slide()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_strength
+	else:
+		if Input.is_action_just_released("jump") and velocity.y < jump_strength / 2:
+			velocity.y = jump_strength / 2

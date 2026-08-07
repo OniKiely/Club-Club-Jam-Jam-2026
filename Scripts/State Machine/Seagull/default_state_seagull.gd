76ae99ba6a5b793eb06847b0e7c0ed5687ca1579
Lily in the Wind <aquietlilyinthewind@gmail.com>
2026-08-07 01:12:34 -0700

    Seagull mostly done

--- Scripts/State Machine/Seagull/default_state_seagull.gd
+++ Scripts/State Machine/Seagull/default_state_seagull.gd
@@ -1,13 +1,21 @@
 extends state
 
+var dir = Vector2(1, 0)
+var speed = 50
+
 #calls once when the state is switched to
 func enter() -> void:
-	pass
+	$FlipTimer.start()
+	get_parent().get_parent().velocity = dir * speed
 
 #called each frame, but only when the state is active
 func run(delta: float) -> void:
-	pass
+	pass #main seagull already does move_and_slide()
 
 #called when the state is switched out of
 func exit() -> void:
-	pass
+	$FlipTimer.stop()
+
+func _on_flip_timer_timeout() -> void:
+	dir.x *= -1
+	get_parent().get_parent().velocity = dir * speed
--- Scripts/seagull.gd
+++ Scripts/seagull.gd
@@ -1,4 +1,23 @@
 extends CharacterBody2D
 
+var player = null
+
 func _ready() -> void:
 	pass
+
+func _process(delta: float) -> void:
+	if self.velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
+	move_and_slide() #StateMachine handles the velocity 
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	$StateMachine.switch_to("Follow")
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	$StateMachine.switch_to("Wander")

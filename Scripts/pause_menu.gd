c1670ef569c51bbfdaa66eb8bcf040da6432be13
MLJoson <littlemjoson@gmail.com>
2026-08-07 00:58:29 -0700

    Added pause ui and functions

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func _ready() -> void:
+	get_tree().paused = false
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testPause():
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+
+
+func _on_restart_button_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _process(_delta):
+	testPause()

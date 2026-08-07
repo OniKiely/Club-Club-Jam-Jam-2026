0f9767661488ec55977ba19e5b849b141d25e728
Cheap4Royal <1234abby12345@gmail.com>
2026-08-07 01:08:26 -0700

    menu changes

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -15,4 +15,4 @@ func _on_exit_button_pressed() -> void:
 
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start":
-		get_tree().change_scene_to_file("res://Scenes/shootingTest.tscn")
+		get_tree().change_scene_to_file("res://Scenes/testWorld.tscn")
